4b022ee93f609eac04e5553076fac682be993113
renedogj <renedogj@gmail.com>
2022-11-18 15:13:45 +0100

    fix estilos votar el ganador

--- controllers/clasificatoria.php
+++ controllers/clasificatoria.php
@@ -24,34 +24,39 @@ if(!isset($_SESSION["id"])){
 	<link rel="stylesheet" type="text/css" href="../css/menu.css">
 	<link rel="stylesheet" type="text/css" href="../css/tablas.css">
 	<link rel="stylesheet" type="text/css" href="../css/partidos.css">
+	<link rel="stylesheet" type="text/css" href="../css/clasificatoria.css">
 </head>
 <body>
 	<nav class="nav-menu" id="menu"></nav>
 	<h1>Porra clasificación mundial Qatar 2022</h1>
 	<div class="divContenedoraInputPuesto">
-		<div class="divInputPuesto">
-			<label for="puesto_1">Primer puesto</label>
-			<select id="puesto_1" name="puesto_1" class="selectPaises">
-				<option>--</option>
-			</select>
+		<div class="divInput_1_2_puesto">
+			<div class="divInputPuesto">
+				<label for="puesto_1">Primer puesto</label>
+				<select id="puesto_1" name="puesto_1" class="selectPaises">
+					<option>--</option>
+				</select>
+			</div>
+			<div class="divInputPuesto">
+				<label for="puesto_2">Segundo puesto</label>
+				<select id="puesto_2" name="puesto_2" class="selectPaises">
+					<option>--</option>
+				</select>
+			</div>
 		</div>
-		<div class="divInputPuesto">
-			<label for="puesto_2">Segundo puesto</label>
-			<select id="puesto_2" name="puesto_2" class="selectPaises">
-				<option>--</option>
-			</select>
-		</div>
-		<div class="divInputPuesto">
-			<label for="puesto_3">Tercer puesto</label>
-			<select id="puesto_3" name="puesto_3" class="selectPaises">
-				<option>--</option>
-			</select>
-		</div>
-		<div class="divInputPuesto">
-			<label for="puesto_4">Cuarto puesto</label>
-			<select id="puesto_4" name="puesto_4" class="selectPaises">
-				<option>--</option>
-			</select>
+		<div class="divInput_3_4_puesto">
+			<div class="divInputPuesto">
+				<label for="puesto_3">Tercer puesto</label>
+				<select id="puesto_3" name="puesto_3" class="selectPaises">
+					<option>--</option>
+				</select>
+			</div>
+			<div class="divInputPuesto">
+				<label for="puesto_4">Cuarto puesto</label>
+				<select id="puesto_4" name="puesto_4" class="selectPaises">
+					<option>--</option>
+				</select>
+			</div>
 		</div>
 	</div>
 	<button id="guardarPorraClasificacion" class="button">Guardar</button>
