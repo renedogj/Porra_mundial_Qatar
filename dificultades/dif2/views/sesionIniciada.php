94d0407a59c834cd516c4f7cadd55d269ae26962
renedogj <renedogj@gmail.com>
2024-02-27 16:58:26 +0100

    fix branches

--- dificultades/dif2/views/sesionIniciada.php
+++
@@ -1,5 +0,0 @@
-<div class="sesionIniciada" id="sesionIniciada">
-	<h1>Bienvenido <?php echo $_SESSION["nombre"]?></h1>
-	<h3>Tienes <?php echo $_SESSION["puntuacion"]?> puntos</h3>
-	<button class="button" id="bttn-cerrarSesion">Cerrar Sesión</button>
-</div>
--- dificultades/dif3/models/obtenerPaises.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "../db/db.php";
-
-$sql = "SELECT id, nombre, abreviatura, grupo from paises order by grupo";
-
-$partidos = obtenerArraySQL($conexion, $sql);
-
-echo json_encode($partidos);
-?>
