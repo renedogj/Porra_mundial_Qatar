88b4f2f1e2016bd3fb636f1d91957e2dd8a7da8d
renedogj <renedogj@gmail.com>
2024-04-04 02:48:48 +0200

    fix cerrarSesion, fix css

--- models/cerrarSesion.php
+++ models/cerrarSesion.php
@@ -4,18 +4,18 @@ session_start();
 session_unset();
 session_destroy();
 
-setcookie("id", "", time() - 3600, "/mundial%20qatar/dificultades/dif1/");
-setcookie("nombre", "", time() - 3600, "/mundial%20qatar/dificultades/dif1/");
-setcookie("puntuacion", "", time() - 3600, "/mundial%20qatar/dificultades/dif1/");
-setcookie("email", "", time() - 3600, "/mundial%20qatar/dificultades/dif1/");
+setcookie("id", "", time() - 3600, "dificultades/dif1/");
+setcookie("nombre", "", time() - 3600, "dificultades/dif1/");
+setcookie("puntuacion", "", time() - 3600, "dificultades/dif1/");
+setcookie("email", "", time() - 3600, "dificultades/dif1/");
 
-setcookie("id", "", time() - 3600, "/mundial%20qatar/dificultades/dif2/");
-setcookie("nombre", "", time() - 3600, "/mundial%20qatar/dificultades/dif2/");
-setcookie("puntuacion", "", time() - 3600, "/mundial%20qatar/dificultades/dif2/");
-setcookie("email", "", time() - 3600, "/mundial%20qatar/dificultades/dif2/");
+setcookie("id", "", time() - 3600, "dificultades/dif2/");
+setcookie("nombre", "", time() - 3600, "dificultades/dif2/");
+setcookie("puntuacion", "", time() - 3600, "dificultades/dif2/");
+setcookie("email", "", time() - 3600, "dificultades/dif2/");
 
-setcookie("id", "", time() - 3600, "/mundial%20qatar/dificultades/dif3/");
-setcookie("nombre", "", time() - 3600, "/mundial%20qatar/dificultades/dif3/");
-setcookie("puntuacion", "", time() - 3600, "/mundial%20qatar/dificultades/dif3/");
-setcookie("email", "", time() - 3600, "/mundial%20qatar/dificultades/dif3/");
+setcookie("id", "", time() - 3600, "dificultades/dif3/");
+setcookie("nombre", "", time() - 3600, "dificultades/dif3/");
+setcookie("puntuacion", "", time() - 3600, "dificultades/dif3/");
+setcookie("email", "", time() - 3600, "dificultades/dif3/");
 ?>
